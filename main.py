48c393b118aa758b9f4bbf4aa16d13f9434915ad
      ///// <erIkgrenc60@gmail.com>
2025-06-10 20:55:36 +0200

    0.0.4

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def generate_ball():
    global ball_y
    ball_x = random.randint(0,WIDTH)
    ball_y = random.randint(0,HEIGHT)
-   red = random.randint(0,255)
-   green = random.randint(0,255)
-   blue = random.randint(0,255)
+   red = random.randint(100,255)
+   green = random.randint(100,255)
+   blue = random.randint(100,255)
    pygame.draw.circle(
        main_screen,
        (red,green,blue),
@@ -18,7 +18,7 @@ def generate_ball():
    )
    pygame.display.update()
 
-
+score=0
 WIDTH = 800
 HEIGHT = 800
 
@@ -27,12 +27,23 @@ main_screen=pygame.display.set_mode((WIDTH,HEIGHT))
 
 ball_x = ball_y = 0
 
+clock = pygame.time.Clock()
+
 while True:
-    generate_ball()
+    main_screen.fill((0,0,0))
+    clock.tick(1)
     for event in pygame.event.get():
-        print(event)
+        # print(event)
         if event.type == pygame.QUIT:
             pygame.quit()
-        if event.type == pygame.MOUSEMOTION:
-            pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x=event.pos[0]
+            mouse_y=event.pos[1]
+            print(mouse_x, mouse_y)
+            if ball_x - 20 <=mouse_x <= ball_x + 20 and ball_y - 20 <=mouse_y <= ball_y + 20:
+                score+=1
+
+
+
+    generate_ball()
     pygame.display.update()
