91d4e6a94f7e8a2b17c78a280398b05b70eef86a
      ///// <erIkgrenc60@gmail.com>
2025-06-10 19:57:37 +0200

    0.0.1

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+WIDTH = 800
+HEIGHT = 800
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+pygame.init()
+main_screen=pygame.display.set_mode((WIDTH,HEIGHT))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/dscsdc
+while True:
+    pass
