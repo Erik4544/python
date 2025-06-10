1428ad14f7685f551cbb25cbc332c48f37a9f2da
      ///// <erIkgrenc60@gmail.com>
2025-06-10 20:13:11 +0200

    0.0.2

--- main.py
+++ main.py
@@ -11,4 +11,17 @@ pygame.init()
 main_screen=pygame.display.set_mode((WIDTH,HEIGHT))
 
 while True:
-    pass
+    for event in pygame.event.get():
+        print(event)
+        if event.type==pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEMOTION:
+            x=event.pos[0]
+            y=event.pos[1]
+            pygame.draw.circle(
+                main_screen,
+                (0,255,255),
+                (x,y),
+                10,
+            )
+    pygame.display.update()
